8804c7b7cf20fa7ad8def7e69761b09928dcd628
Julio Freitas <juliofreitas@sisconsultoria.com.br>
2023-10-29 13:48:45 -0300

    Portifolio extensão de arquivo de imagem

--- src/app/components/skill/skill.tsx
+++ src/app/components/skill/skill.tsx
@@ -11,12 +11,12 @@ interface SkillProps {
 export function Skill(props : SkillProps){
     return(
         <div className="experience-language">
-            <Image
+            <img
                 src={props.image}
                 alt="Skill Logo"
                 width={40}
                 height={40}
-                priority
+                
             />
             <div className="experience-unit">
                 <div className={`experience-measure measure-${props.measure}`}>
